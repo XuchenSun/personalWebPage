62f6823027c0419d0be33132c0eb495a5fddfeb2
sunxu <xuchens@mun.ca>
2021-07-05 21:06:55 -0230

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my Personal Web Page
+          Hello World From Xuchen Personal Page
         </p>
         <a
           className="App-link"
